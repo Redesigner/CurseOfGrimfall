423ebf1d9353b9c9d666d47ff5743885ae1071fd
Redesigner <stephenwmelnick@gmail.com>
2023-06-21 21:04:33 -0400

    PushableBlock WIP

--- Source/Foci/Actors/Objects/PushableBlock.cpp
+++ Source/Foci/Actors/Objects/PushableBlock.cpp
@@ -6,6 +6,8 @@
 
 APushableBlock::APushableBlock()
 {
+	PrimaryActorTick.bCanEverTick = true;
+
 	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Block"));
 	RootComponent = Box;
 
@@ -16,6 +18,28 @@ APushableBlock::APushableBlock()
 void APushableBlock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (!SnapToFloor())
+	{
+		Velocity += FVector(0.0f, 0.0f, GetWorld()->GetGravityZ() * DeltaTime);
+	}
+	const FVector Delta = Velocity * DeltaTime;
+
+	FHitResult MoveHitResult;
+	const FVector StartLocation = Box->GetComponentLocation();
+	const FVector EndLocation = StartLocation + Delta;
+	FCollisionQueryParams CollisionQueryParams;
+	CollisionQueryParams.AddIgnoredActor(this);
+
+	GetWorld()->SweepSingleByProfile(MoveHitResult, StartLocation, EndLocation, Box->GetComponentQuat(), Box->GetCollisionProfileName(), Box->GetCollisionShape(1.0f), CollisionQueryParams);
+
+	if (MoveHitResult.bBlockingHit)
+	{
+		AddActorWorldOffset(MoveHitResult.Location - StartLocation, false);
+		return;
+	}
+	AddActorWorldOffset(Delta);
+	
 }
 
 void APushableBlock::Push(FVector Delta, AActor* Source, FHitResult& HitResult)
@@ -26,10 +50,11 @@ void APushableBlock::Push(FVector Delta, AActor* Source, FHitResult& HitResult)
 	CollisionQueryParams.AddIgnoredActor(this);
 	CollisionQueryParams.AddIgnoredActor(Source);
 
-	GetWorld()->SweepSingleByProfile(HitResult, StartLocation, EndLocation, Box->GetComponentQuat(), Box->GetCollisionProfileName(), Box->GetCollisionShape(1.0f), CollisionQueryParams);
+	GetWorld()->SweepSingleByProfile(HitResult, StartLocation, EndLocation, Box->GetComponentQuat(), Box->GetCollisionProfileName(), Box->GetCollisionShape(0.0f), CollisionQueryParams);
 	
-	if (HitResult.bBlockingHit)
+	if (HitResult.IsValidBlockingHit())
 	{
+		UE_LOG(LogTemp, Display, TEXT("Pushing block, but block hit object '%s'"), *HitResult.GetActor()->GetFName().ToString())
 		if (AFloorButton* FloorButton = Cast<AFloorButton>(HitResult.GetActor()))
 		{
 			FloorButton->Trigger(Source);
@@ -40,6 +65,10 @@ void APushableBlock::Push(FVector Delta, AActor* Source, FHitResult& HitResult)
 			AddActorWorldOffset(HitResult.Location - StartLocation);
 		}
 	}
+	else
+	{
+		AddActorWorldOffset(Delta);
+	}
 }
 
 UBoxComponent* APushableBlock::GetBlockComponent() const
@@ -49,10 +78,50 @@ UBoxComponent* APushableBlock::GetBlockComponent() const
 
 bool APushableBlock::SnapToFloor()
 {
+	const float SnapHeight = 10.0f;
+
+	FHitResult FloorHitResult;
+	const FVector StartLocation = Box->GetComponentLocation();
+	const FVector EndLocation = StartLocation - FVector(0.0f, 0.0f, SnapHeight);
+	FCollisionQueryParams CollisionQueryParams;
+	CollisionQueryParams.AddIgnoredActor(this);
+
+	GetWorld()->SweepSingleByProfile(FloorHitResult, StartLocation, EndLocation, Box->GetComponentQuat(), Box->GetCollisionProfileName(), Box->GetCollisionShape(1.0f), CollisionQueryParams);
+	
+	if (FloorHitResult.bBlockingHit)
+	{
+		AddActorWorldOffset(FloorHitResult.Location - StartLocation, false);
+		if (AFloorButton* FloorButton = Cast<AFloorButton>(FloorHitResult.GetActor()))
+		{
+			FloorButton->Trigger(this);
+		}
+		Velocity.Z = 0.0f;
+		return true;
+	}
+
 	return false;
 }
 
+// This also moves the block over the button, if need be
 bool APushableBlock::SnapToButton(AFloorButton* FloorButton, FVector Delta)
 {
+	const float SnapHeight = 250.0f;
+	// This assumes that the move would've been successful, if not for the button being in the way
+	FHitResult ButtonSnapResult;
+	const FVector EndLocation = Box->GetComponentLocation() + Delta;
+	const FVector StartLocation = EndLocation + FVector(0.0f, 0.0f, SnapHeight);
+	
+	UBoxComponent* ButtonSurface = FloorButton->GetSurface();
+
+	ButtonSurface->SweepComponent(ButtonSnapResult, StartLocation, EndLocation, Box->GetComponentQuat(), Box->GetCollisionShape(1.0f));
+
+	if (ButtonSnapResult.bBlockingHit)
+	{
+		AddActorWorldOffset(ButtonSnapResult.Location - Box->GetComponentLocation());
+		return true;
+	}
+
+	// The sweep failed! This can sometimes happen, so just move the block here
+	AddActorWorldOffset(Delta);
 	return false;
 }
